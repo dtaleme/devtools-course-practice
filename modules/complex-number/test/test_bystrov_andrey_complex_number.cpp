8115d49a8ad0666d584719788e11c1f8c21e3f94
dtaleme <andr-bis2013@ya.ru>
2022-10-26 22:28:38 +0300

    more tests

--- modules/complex-number/test/test_bystrov_andrey_complex_number.cpp
+++ modules/complex-number/test/test_bystrov_andrey_complex_number.cpp
@@ -17,3 +17,55 @@ TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, ParameterizedConstructor) {
 TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, CopyConstructor) {
   ASSERT_NO_THROW(ComplexNumber(ComplexNumber()));
 }
+
+TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, AssignmentOperator) {
+  auto origin = ComplexNumber(1.0, 2.0);
+  auto copy = ComplexNumber();
+  ASSERT_NO_THROW(copy = origin);
+}
+
+TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, EqualityOperator) {
+  auto first = ComplexNumber(1.0, 2.0);
+  auto second = ComplexNumber(1.0, 2.0);
+  ASSERT_EQ(first, second);
+}
+
+TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, CopyEqualsToOrigin) {
+  auto origin = ComplexNumber(1.0, 2.0);
+  auto copy = origin;
+  ASSERT_EQ(origin, copy);
+}
+
+TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, InequalityOperator) {
+  auto first = ComplexNumber(1.0, 2.0);
+  auto second = ComplexNumber(3.0, 4.0);
+  ASSERT_NE(first, second);
+}
+
+TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, AdditionOperator) {
+  auto first = ComplexNumber(1.0, 2.0);
+  auto second = ComplexNumber(3.0, 4.0);
+  auto product = first + second;
+  ASSERT_EQ(product, ComplexNumber(4.0, 6.0));
+}
+
+TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, SubtractionOperator) {
+  auto first = ComplexNumber(1.0, 2.0);
+  auto second = ComplexNumber(3.0, 4.0);
+  auto product = first - second;
+  ASSERT_EQ(product, ComplexNumber(-2.0, -2.0));
+}
+
+TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, MultiplicationOperator) {
+  auto first = ComplexNumber(1.0, 2.0);
+  auto second = ComplexNumber(3.0, 4.0);
+  auto product = first * second;
+  ASSERT_EQ(product, ComplexNumber(-5.0, 10.0));
+}
+
+TEST(BYSTROV_ANDREY_COMPLEX_NUMBER_TEST, DivisionOperator) {
+  auto first = ComplexNumber(1.0, 2.0);
+  auto second = ComplexNumber(3.0, 4.0);
+  auto product = first / second;
+  ASSERT_EQ(product, ComplexNumber(0.44, 0.08));
+}
