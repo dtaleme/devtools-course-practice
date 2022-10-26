ef9670e3e10a449d85e137361e861b292c140e89
dtaleme <andr-bis2013@ya.ru>
2022-10-26 22:37:44 +0300

    first commit

---
+++ modules/bystrov_a_vigenere_cipher/include/vigenere_cipher.h
@@ -0,0 +1,27 @@
+// Copyright 2022 Bystrov Andrey
+
+#ifndef MODULES_BYSTROV_A_VIGENERE_CIPHER_INCLUDE_VIGENERE_CIPHER_H_
+#define MODULES_BYSTROV_A_VIGENERE_CIPHER_INCLUDE_VIGENERE_CIPHER_H_
+
+#include <string>
+
+class VigenereCipher {
+  std::string m_original;
+  std::string m_key;
+
+ public:
+  VigenereCipher();
+  VigenereCipher(const std::string& original, const std::string& key);
+
+  const std::string& getOriginal() const;
+  const std::string& getKey() const;
+
+  void setOriginal(const std::string& original);
+  void setKey(const std::string& key);
+
+  void setPair(const std::string& original, const std::string& key);
+
+  std::string cipher() const;
+};
+
+#endif  // MODULES_BYSTROV_A_VIGENERE_CIPHER_INCLUDE_VIGENERE_CIPHER_H_
