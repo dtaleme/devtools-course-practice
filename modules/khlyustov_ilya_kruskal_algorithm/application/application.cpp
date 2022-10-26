bd731442157e02b3978064716bc840c2b2827948
dtaleme <andr-bis2013@ya.ru>
2022-10-26 22:42:33 +0300

    app

---
+++ modules/khlyustov_ilya_kruskal_algorithm/application/application.cpp
@@ -0,0 +1,12 @@
+// Copyright 2022 Bystrov Andrey
+
+#include <iostream>
+#include <string>
+#include "include/kruskal_app.h"
+
+int main(int argc, const char** argv) {
+  CLPApplication app;
+  std::string output = app(argc, argv);
+  std::cout << output << std::endl;
+  return 0;
+}
