8f28eef7d9ccbd6a33c64b20eee70f3f4fc05df9
dtaleme <andr-bis2013@ya.ru>
2022-10-26 23:28:10 +0300

    fix

--- modules/khlyustov_ilya_kruskal_algorithm/src/kruskal_app.cpp
+++ modules/khlyustov_ilya_kruskal_algorithm/src/kruskal_app.cpp
@@ -41,10 +41,16 @@ std::string CLPApplication::operator()(int argc, const char** argv) {
     std::vector<Edge> edges = {
         Edge(atoi(argv[3]), atoi(argv[4]), atoi(argv[5])),
         Edge(atoi(argv[6]), atoi(argv[7]), atoi(argv[8])),
+        Edge(atoi(argv[9]), atoi(argv[10]), atoi(argv[11])),
+        Edge(atoi(argv[12]), atoi(argv[13]), atoi(argv[14])),
+        Edge(atoi(argv[15]), atoi(argv[16]), atoi(argv[17])),
+        Edge(atoi(argv[18]), atoi(argv[19]), atoi(argv[20])),
     };
+
     Graph testGraph(nodes, edges);
-    // EXPECT_EQ(edges, testGraph.getMST());
-    os << testGraph.getMST().size();
+
+    // EXPECT_EQ(static_cast<size_t>(5), testGraph.getNodesCount());
+    os << testGraph.getNodesCount();
     std::cout << os.str() << std::endl;
     return "success";
   }
@@ -53,10 +59,16 @@ std::string CLPApplication::operator()(int argc, const char** argv) {
     std::vector<Edge> edges = {
         Edge(atoi(argv[3]), atoi(argv[4]), atoi(argv[5])),
         Edge(atoi(argv[6]), atoi(argv[7]), atoi(argv[8])),
+        Edge(atoi(argv[9]), atoi(argv[10]), atoi(argv[11])),
+        Edge(atoi(argv[12]), atoi(argv[13]), atoi(argv[14])),
+        Edge(atoi(argv[15]), atoi(argv[16]), atoi(argv[17])),
+        Edge(atoi(argv[18]), atoi(argv[19]), atoi(argv[20])),
     };
+
     Graph testGraph(nodes, edges);
-    // EXPECT_EQ(edges, testGraph.getMST());
-    os << testGraph.getMST().size();
+
+    // EXPECT_EQ(static_cast<size_t>(6), testGraph.getEdgesCount());
+    os << testGraph.getEdgesCount();
     std::cout << os.str() << std::endl;
     return "success";
   }
